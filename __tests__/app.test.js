70e3a67aa925b6bf8122f5cbb1acaa1b83590990
zahraakarim <nikishakarim@gmail.com>
2023-06-28 13:00:38 +0100

    completed ticket 6

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,125 +1,174 @@
-const request = require('supertest');
-const app = require('../app');
-const db = require('../db/connection')
-const seed = require('../db/seeds/seed')
-const data = require('../db/data/test-data')
-const endpoints = require('../endpoints.json')
+const request = require("supertest");
+const app = require("../app");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+const endpoints = require("../endpoints.json");
 
 beforeEach(() => {
-    return seed(data);
+  return seed(data);
+});
+
+afterAll(() => {
+  return db.end();
+});
+
+describe("GET /api/topics", () => {
+  test("200: GET - responds with an array of topic objects, each of which should have a property of slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        const { topics } = body;
+        expect(Array.isArray(topics)).toBe(true);
+        expect(topics).toHaveLength(3);
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug", expect.any(String));
+          expect(topic).toHaveProperty("description", expect.any(String));
+        });
+      });
   });
-  
-  afterAll(() => {
-    return db.end();
+  test("404: ERROR - responds with an error when an invalid endpoint has been inputted", () => {
+    return request(app)
+      .get("/api/nonesense")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Path not found");
+      });
   });
+});
 
-describe('GET /api/topics', () => {
-    test("200: GET - responds with an array of topic objects, each of which should have a property of slug and description", () => {
-        return request(app)
-        .get('/api/topics')
-        .expect(200)
-        .then(({body}) => {
-            const {topics} = body
-            expect(Array.isArray(topics)).toBe(true);
-            expect(topics).toHaveLength(3);
-            topics.forEach((topic) => {
-                expect(topic).toHaveProperty("slug", expect.any(String));
-                expect(topic).toHaveProperty("description", expect.any(String));
+describe("GET /api", () => {
+  test("200: GET: responds with an object describing all the available endpoints on my API", () => {
+    request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(endpoints);
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: GET: responds with an article object with properties of author, title, article_id, body, topic, created_at, votes, article_img_url", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        const { article } = body;
+        expect(article).toHaveProperty("author", expect.any(String));
+        expect(article).toHaveProperty("title", expect.any(String));
+        expect(article).toHaveProperty("article_id", expect.any(Number));
+        expect(article).toHaveProperty("body", expect.any(String));
+        expect(article).toHaveProperty("topic", expect.any(String));
+        expect(article).toHaveProperty("created_at", expect.any(String));
+        expect(article).toHaveProperty("votes", expect.any(Number));
+        expect(article).toHaveProperty("article_img_url", expect.any(String));
+      });
+  });
+});
+test("404: ERROR: responds with an error when article_id is valid, but does not exist", () => {
+  return request(app)
+    .get("/api/articles/999999")
+    .expect(404)
+    .then(({ body }) => {
+      expect(body.msg).toBe("Path not found");
+    });
+});
+test("400: ERROR: responds with an error when article id is an invalid type", () => {
+  return request(app)
+    .get("/api/articles/nonesense")
+    .expect(400)
+    .then(({ body }) => {
+      expect(body.msg).toBe("Bad Request");
+    });
+});
+
+describe("GET /api/articles", () => {
+  test("200: GET: responds with an articles array of article objects, each of which should have the properties author, title, article_id, topic, created_at, votes, article_img_url, comment_count", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        expect(articles).toHaveLength(13);
+        articles.forEach((article) => {
+          expect(article).toHaveProperty("author", expect.any(String));
+          expect(article).toHaveProperty("title", expect.any(String));
+          expect(article).toHaveProperty("article_id", expect.any(Number));
+          expect(article).toHaveProperty("topic", expect.any(String));
+          expect(article).toHaveProperty("created_at", expect.any(String));
+          expect(article).toHaveProperty("votes", expect.any(Number));
+          expect(article).toHaveProperty("article_img_url", expect.any(String));
+          expect(article).toHaveProperty("comment_count", expect.any(Number));
+          expect(article).not.toHaveProperty("body", expect.any(String));
+        });
+      });
+  });
+  test("200: GET: responds with array of article objects sorted in descending order by date", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        expect(articles).toBeSortedBy("created_at", { descending: true });
+      });
+  });
+  test("404: ERROR: responds with an error when path is valid but does not exist", () => {
+    return request(app)
+      .get("/api/nonesense")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Path not found");
+      });
+  });
+});
+
+describe("GET /api/articles/:article_id/comments", () => {
+  test("200: GET: an array of comments for the given article_id of which each comment has the properties comment_id, votes, created_at, author, body, article_id", () => {
+    return request(app)
+      .get("/api/articles/1/comments")
+      .expect(200)
+      .then(({ body }) => {
+        const { comments } = body;
+        expect(comments).toHaveLength(11);
+        expect(comments).toBeSortedBy("created_at", { descending: true });
+        comments.forEach((comment) => {
+          expect(comment).toEqual(
+            expect.objectContaining({
+              comment_id: expect.any(Number),
+              votes: expect.any(Number),
+              created_at: expect.any(String),
+              author: expect.any(String),
+              body: expect.any(String),
+              article_id: expect.any(Number),
             })
-        })
-    })
-    test("404: ERROR - responds with an error when an invalid endpoint has been inputted", () => {
-        return request(app)
-        .get('/api/nonesense')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe('Path not found')
-        })
-    })
-})
-
-describe('GET /api', () => {
-    test("200: GET: responds with an object describing all the available endpoints on my API", () => {
-        request(app)
-        .get('/api')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.endpoints).toEqual(endpoints)
-        })
-    })
-})
-
-describe('GET /api/articles/:article_id', () => {
-    test("200: GET: responds with an article object with properties of author, title, article_id, body, topic, created_at, votes, article_img_url", () => {
-        return request(app)
-        .get('/api/articles/1')
-        .expect(200)
-        .then(({body}) => {
-            const { article } = body;
-            expect(article).toHaveProperty('author', expect.any(String));
-            expect(article).toHaveProperty('title', expect.any(String));
-            expect(article).toHaveProperty('article_id', expect.any(Number));
-            expect(article).toHaveProperty('body', expect.any(String));
-            expect(article).toHaveProperty('topic', expect.any(String));
-            expect(article).toHaveProperty('created_at', expect.any(String));
-            expect(article).toHaveProperty('votes', expect.any(Number));
-            expect(article).toHaveProperty('article_img_url', expect.any(String));
-            })
-        })
-    })
-    test("404: ERROR: responds with an error when article_id is valid, but does not exist", () => {
-        return request(app)
-        .get('/api/articles/999999')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe('Path not found')
-        })
-    })
-    test("400: ERROR: responds with an error when article id is an invalid type", () => {
-        return request(app)
-        .get('/api/articles/nonesense')
-        .expect(400)
-        .then(({body}) => {
-            expect(body.msg).toBe('Bad Request')
-        })
-    })
-
-describe('GET /api/articles', () => {
-    test("200: GET: responds with an articles array of article objects, each of which should have the properties author, title, article_id, topic, created_at, votes, article_img_url, comment_count", () => {
-        return request(app)
-        .get('/api/articles')
-        .expect(200)
-        .then(({body}) => {
-            const {articles} = body
-            expect(articles).toHaveLength(13);
-            articles.forEach((article) => {
-                expect(article).toHaveProperty("author", expect.any(String));
-                expect(article).toHaveProperty("title", expect.any(String));
-                expect(article).toHaveProperty("article_id", expect.any(Number));
-                expect(article).toHaveProperty("topic", expect.any(String));
-                expect(article).toHaveProperty("created_at", expect.any(String));
-                expect(article).toHaveProperty("votes", expect.any(Number));
-                expect(article).toHaveProperty("article_img_url", expect.any(String));
-                expect(article).toHaveProperty("comment_count", expect.any(Number));
-                expect(article).not.toHaveProperty("body", expect.any(String));
-            })
-        })
-    })
-    test("200: GET: responds with array of article objects sorted in descending order by date", () => {
-        return request(app)
-        .get('/api/articles')
-        .expect(200)
-        .then(({body}) => {
-            const {articles} = body
-            expect(articles).toBeSortedBy("created_at", { descending: true})
-        })
-    })
-    test("404: ERROR: responds with an error when path is valid but does not exist", () => {
-        return request(app)
-        .get('/api/nonesense')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe('Path not found')
-        })
-    })
-})
+          );
+        });
+      });
+  });
+  test("200: GET: responds with an empty array if the article exists but there are no comments ", () => {
+    return request(app)
+      .get("/api/articles/2/comments")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.comments).toHaveLength(0);
+      });
+  });
+  test("400: ERROR: responds with an error when article_id type is invalid", () => {
+    return request(app)
+      .get("/api/articles/nonesense/comments")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
+  test("404: ERROR: responds with an error when article_id type is valid but doesn't exist", () => {
+    return request(app)
+      .get("/api/articles/9999/comments")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Path not found");
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const { getAllTopics, getAllEndpoints, getArticleId, getAllArticles } = require('./news.controllers')
+const {
+  getAllTopics,
+  getAllEndpoints,
+  getArticleId,
+  getAllArticles,
+  getAllArticleComments,
+} = require("./news.controllers");
 
 app.use(express.json());
 
-app.get('/api/topics', getAllTopics)
+app.get("/api/topics", getAllTopics);
 
-app.get('/api', getAllEndpoints)
+app.get("/api", getAllEndpoints);
 
-app.get('/api/articles/:article_id', getArticleId)
+app.get("/api/articles/:article_id", getArticleId);
 
-app.get('/api/articles', getAllArticles)
+app.get("/api/articles", getAllArticles);
+
+app.get("/api/articles/:article_id/comments", getAllArticleComments);
 
 app.all("*", (_, res) => {
-    res.status(404).send({ msg: "Path not found" });
-  });
+  res.status(404).send({ msg: "Path not found" });
+});
 
 app.use((err, req, res, next) => {
-  if(err.code) {
-    res.status(400).send({msg: 'Bad Request'})
+  if (err.code) {
+    res.status(400).send({ msg: "Bad Request" });
   } else next(err);
-})
+});
 
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
@@ -29,7 +37,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    res.status(500).send({msg: 'Internal server error!'})
-})
+  res.status(500).send({ msg: "Internal server error!" });
+});
 
 module.exports = app;
