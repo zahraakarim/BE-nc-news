676090cae52d12de7e4a8b22fa12a8d9b1b12043
zahraakarim <nikishakarim@gmail.com>
2023-06-29 11:08:00 +0100

    took out my console log for errors on my listen.js

--- listen.js
+++ listen.js
@@ -2,6 +2,5 @@ const app = require("./app");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
-  if (err) console.log(err);
   console.log(`Server is listening on ${PORT}...`);
 });
